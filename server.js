ecc69cee348857939cb3cf00646fc41053a888a6
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-06-03 01:00:59 -0500

    Restart hw due to database not working offline

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  useFindAndModify: false
 });
 
 // routes
